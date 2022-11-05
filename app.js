c29309c366b7bc3bb761bd2823f2a36e48be856d
haze272 <studiorain27@gmail.com>
2022-11-05 23:44:08 +0300

    Данные

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
 let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
-let pavelPizzaRouter = require('./routes/pavelPizza');
 let itedCourseRouter = require('./routes/itedCourse');
 
 let app = express();
@@ -22,24 +20,22 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/pavel-pizza', pavelPizzaRouter);
 app.use('/ited-api', itedCourseRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).render('error');
 });
 
 module.exports = app;
